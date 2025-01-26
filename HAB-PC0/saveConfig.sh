fc7d228258a3fcccc80e3e19a4ff070bb7481eb9
DrX7FFF <dubourg.v@gmail.com>
2025-01-26 18:53:31 +0100

    Sauvegarde config HABPC

--- HAB-PC0/saveConfig.sh
+++ HAB-PC0/saveConfig.sh
@@ -6,6 +6,7 @@ mkdir -p ./config/ssh
 # Ubuntu 24.04
 dconf dump / > ./config/gnome_settings.dconf
 cp ~/.zshrc ./config/.zshrc
+cp -r ~/.oh-my-zsh ./config/.oh-my-zsh
 cp ~/.ssh/config ./config/ssh/config
 
 # restore with
