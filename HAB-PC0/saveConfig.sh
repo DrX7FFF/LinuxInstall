82f185a4b82054446a05b1b3156edaa3252c3908
DrX7FFF <dubourg.v@gmail.com>
2025-08-22 18:33:40 +0200

    Sauvegarde

--- HAB-PC0/saveConfig.sh
+++ HAB-PC0/saveConfig.sh
@@ -2,12 +2,14 @@
 
 mkdir -p ./config
 mkdir -p ./config/ssh
+mkdir  -p ./config/nautilus_scripts
 
 # Ubuntu 24.04
 dconf dump / > ./config/gnome_settings.dconf
 cp ~/.zshrc ./config/.zshrc
 cp -r ~/.oh-my-zsh ./config/.oh-my-zsh
 cp ~/.ssh/config ./config/ssh/config
+cp ~/.local/share/nautilus/scripts/* ./config/nautilus_scripts/
 
 # restore with
 # dconf load -f / < saved_settings.dconf
