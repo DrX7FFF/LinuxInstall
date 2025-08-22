bc98e94ff63a5e523a2e83b0f21f23d3eaf2024a
DrX7FFF <dubourg.v@gmail.com>
2025-08-22 18:45:59 +0200

    suppression bakup .oh-my-zsh

--- HAB-PC0/saveConfig.sh
+++ HAB-PC0/saveConfig.sh
@@ -7,9 +7,11 @@ mkdir  -p ./config/nautilus_scripts
 # Ubuntu 24.04
 dconf dump / > ./config/gnome_settings.dconf
 cp ~/.zshrc ./config/.zshrc
-cp -r ~/.oh-my-zsh ./config/.oh-my-zsh
 cp ~/.ssh/config ./config/ssh/config
 cp ~/.local/share/nautilus/scripts/* ./config/nautilus_scripts/
+
+# Pas necessaire car généré automatiquement
+# cp -r ~/.oh-my-zsh ./config/.oh-my-zsh
 
 # restore with
 # dconf load -f / < saved_settings.dconf
